4e4e60d37cbc1df65cf14d1d2b601fd6ddef0f04
Einar A <einar.ameen@gmail.com>
2021-12-01 21:11:53 +0100

    Day 1 cleanup

--- src/day1/Program.cs
+++ src/day1/Program.cs
@@ -1,9 +1,41 @@
 ﻿
-var input = File.ReadAllLines("../../input/1input.txt").Select(int.Parse).ToList();
-// var input = File.ReadAllLines("/home/azureuser/aoc/1test.txt").Select(int.Parse).ToList();
+using System.Diagnostics;
+namespace Aoc
+{
+    class Day1
+    {
 
-var part1 = input.Where((x,i) => i > 0 && input[i-1] < x ).Count();
-Console.WriteLine(part1);
+        public static List<int> ReadFileToInt(string name)
+        {
+            return File.ReadAllLines($"input/{name}.txt").Select(int.Parse).ToList();
+        }
+        public static int part1(List<int> input)
+        {
+            return input.Where((x, i) => i > 0 && input[i - 1] < x).Count();
+        }
 
-var part2 = input.Where((x, i) => i>1 && i< input.Count()-1 && input.Skip(i-2).Take(3).Sum() < input.Skip(i-1 ).Take(3).Sum()).ToList().Count();
-Console.WriteLine(part2);
+        public static int part2(List<int> input)
+        {
+            return input.Where((x, i) => i > 1 && i < input.Count() - 1 && input.Skip(i - 2).Take(3).Sum() < input.Skip(i - 1).Take(3).Sum()).ToList().Count();
+        }
+        static void Main(string[] args)
+        {
+
+            Debug.Assert(part1(ReadFileToInt("test")) == 7);
+
+            var part1_result = part1(ReadFileToInt("input"));
+            Console.WriteLine(part1_result); // 1602
+
+            Debug.Assert(part2(ReadFileToInt("test")) == 5);
+
+            var part2_result = part2(ReadFileToInt("input"));
+            Console.WriteLine(part2_result); // 1633
+
+            Debug.Assert(part2(ReadFileToInt("input")) == 1633);
+
+
+        }
+
+    }
+
+}
